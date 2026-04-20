dffc940cd7fcab0b5dfb68ad7313633be599e6b8
Tailer Teilor <tailerteilor@email.com>
2026-04-19 22:59:47 -0300

    Auto pull before push

--- enviar_github.bat
+++ enviar_github.bat
@@ -14,7 +14,7 @@ echo [INFO] Conectando ao repositorio passagensaereas...
 git remote add origin https://github.com/tailerteilor/passagensaereas.git 2>nul
 
 echo [INFO] Lendo arquivo github\version.json...
-powershell -ExecutionPolicy Bypass -Command "$json = Get-Content -Raw -Encoding UTF8 'github\version.json' | ConvertFrom-Json; $commitMsg = 'v' + $json.version + ' - ' + $json.title; Write-Host '=> Subindo:' -ForegroundColor Yellow -NoNewline; Write-Host \" $commitMsg\" -ForegroundColor Cyan; git add .; git commit -m $commitMsg; git branch -M main; git push -u origin main"
+powershell -ExecutionPolicy Bypass -Command "$json = Get-Content -Raw -Encoding UTF8 'github\version.json' | ConvertFrom-Json; $commitMsg = 'v' + $json.version + ' - ' + $json.title; Write-Host '=> Subindo:' -ForegroundColor Yellow -NoNewline; Write-Host \" $commitMsg\" -ForegroundColor Cyan; git add .; git commit -m $commitMsg; git branch -M main; git pull --rebase origin main; git push -u origin main"
 
 echo.
 echo ====================================================
